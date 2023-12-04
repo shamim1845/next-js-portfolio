8953137fbbad508110578d3b4a19310e3a48c591
shamim1845 <samimraj1845@gmail.com>
2023-12-04 07:32:51 +0600

    website_url changed

--- src/app/constant.ts
+++ src/app/constant.ts
@@ -1,6 +1,6 @@
 export const constant = {
   website_name: "SHAMIM",
-  website_url: "https://shamim-me.vercel.app",
+  website_url: "https://www.developershamim.me",
   title: "Web Developer in Bangladesh | Md Shamim Hossain",
   description:
     "I am a web developer in bangladesh, specializing in building exceptional digital experiences. Primarily interested in building web applications using a modern tech stack called MERN (MongoDB, Express, React, Node.js) Stack.",
