d98b5a28935c229cf81b6e644f7959e838c289b3
shamim1845 <samimraj1845@gmail.com>
2025-09-18 23:24:50 +0600

    update title and experience in constant.ts

--- src/app/constant.ts
+++ src/app/constant.ts
@@ -1,7 +1,7 @@
 export const constant = {
   website_name: "SHAMIM",
   website_url: "https://www.developershamim.me",
-  title: "Web Developer in Bangladesh | Md Shamim Hossain",
+  title: "Full Stack JavaScript Developer | Md Shamim Hossain",
   description:
-    "JavaScript Developer with 1+ years of experience building fast, responsive, and scalable web apps using React and Next.js. Passionate about performance, clean code, and great user experiences.",
+    "JavaScript Developer with 2 years of experience building fast, responsive, and scalable web apps using React and Next.js. Passionate about performance, clean code, and great user experiences.",
 };
