1416445eec0768b705c32bc2a6624e0fb34dbaaf
shamim1845 <samimraj1845@gmail.com>
2026-01-25 15:06:38 +0600

    mailcraft project added and resume updated

--- src/app/constant.ts
+++ src/app/constant.ts
@@ -3,5 +3,5 @@ export const constant = {
   website_url: "https://www.developershamim.me",
   title: "Full Stack JavaScript Developer | Md Shamim Hossain",
   description:
-    "JavaScript Developer with 2 years of experience building fast, responsive, and scalable web apps using React and Next.js. Passionate about performance, clean code, and great user experiences.",
+    "JavaScript Developer with 2+ years of experience building fast, responsive, and scalable web apps using React and Next.js. Passionate about performance, clean code, and great user experiences.",
 };
